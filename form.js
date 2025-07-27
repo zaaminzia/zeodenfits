31effd57dc52d1c4a6c1acd3848e001df9d2fd43
Zaamin Zia <150209858+zaaminzia@users.noreply.github.com>
2025-07-27 23:05:50 +0530

    update

--- form.js
+++ form.js
@@ -1,22 +1,26 @@
- 
-// Function to handle form submission and redirection
-function submitForm() {
-    // You can add validation or AJAX submission logic here
-    // For simplicity, let's just display an alert for now
-    alert('Form submitted!');
 
-    // Redirect to the main website
-    window.location.href = "home.html"; // Replace with your main website URL
+function submitForm() {
+ 
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+   
+    if (email.trim() === '' || password.trim() === '') {
+     
+        alert('Please fill in all fields');
+    } else {
+       
+        alert('Form submitted successfully!');
+        window.location.href = 'home.html';
+    }
 }
 
 // Get the form element
 const entryForm = document.getElementById('entryForm');
 
-// Add an event listener for form submission
 entryForm.addEventListener('submit', function(event) {
-    // Prevent the default form submission behavior
+
     event.preventDefault();
 
-    // Call the submitForm function to handle the form submission
     submitForm();
-})
+});
